4b32d6fb5e326a92db6b43ae0fec09cab27daeb8
Sawitree Boonson <sawitree.b@csithailand.com>
2019-10-08 17:28:01 +0700

    [back]

--- jgm/settings.py
+++ jgm/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'member.apps.AccountsConfig',
+    'member',
     'rom'
 ]
 
@@ -131,6 +131,3 @@ STATICFILES_DIRS = (
 )
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'jgm/media/')
-
-LOGIN_REDIRECT_URL = 'home'
-LOGOUT_REDIRECT_URL = 'home'
--- jgm/urls.py
+++ jgm/urls.py
@@ -21,7 +21,4 @@ from django.views.generic.base import TemplateView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url('accounts/', include('member.urls')),
-    url(r'^accounts/', include('django.contrib.auth.urls')),
-    url(r'^', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
