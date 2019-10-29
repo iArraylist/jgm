c5d3c0123abf8855055e2bf6bd533dc049465542
Sawitree Boonson <sawitree.b@csithailand.com>
2019-10-29 11:04:18 +0700

    party modal

---
+++ jgm/static/js/party.js
@@ -0,0 +1,17 @@
+var img_zone_job = document.getElementById('party_card_body');
+
+function party_edit(e){
+console.log('edit')
+};
+
+function party_save(e){
+console.log('save')
+};
+
+function party_back(e){
+console.log('back')
+};
+
+function party_remove(e){
+console.log('remove')
+};
