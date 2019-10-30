dca5da96b44482388295fc03aaace751e07dc9b2
Sawitree Boonson <aimd.sawitree@gmail.com>
2019-10-30 12:39:36 +0700

    [edit]

--- jgm/static/js/character.js
+++ jgm/static/js/character.js
@@ -6,18 +6,21 @@ var img_woc_job = document.getElementById('img_woc_job');
 var img_zone_job = document.getElementById('img_zone_job');
 
 function sl_change(e){
-    lb = document.getElementById(e.getAttribute('lb'));
+    lb = document.getElementById(e.dataset.lb);
     var jobs = document.getElementsByName('jobs');
     var index;
     for (index = 0; index < jobs.length; index++){
         if (jobs[index].type == 'checkbox') {
             if(jobs[index].value == e.value){
-                var model_img = document.getElementById(jobs[index].getAttribute('img_id'));
+                var model_img = document.getElementById(jobs[index].dataset.img_id);
                 var img = model_img.cloneNode(true);
-                img.setAttribute('id', '');
-                img.setAttribute('class', '');
+                img.removeAttribute('id');
+                img.removeAttribute('class');
                 img.setAttribute('width', 24);
                 img.setAttribute('height', 24);
+                while (lb.hasChildNodes()) {
+                  lb.removeChild(lb.firstChild);
+                }
                 lb.appendChild(img);
             }
         }
@@ -26,12 +29,10 @@ function sl_change(e){
 
 function cb_change(e){
     if (e.checked ) {
-        console.log('check');
-        img = document.getElementById(e.getAttribute('img_id')).setAttribute('class', '');
+        img = document.getElementById(e.dataset.img_id).setAttribute('class', '');
     }
     else {
-        console.log('uncheck');
-        img = document.getElementById(e.getAttribute('img_id')).setAttribute('class', 'grayscale');
+        img = document.getElementById(e.dataset.img_id).setAttribute('class', 'grayscale');
     }
     render_option(woe_fixable, img_woe_job);
     render_option(woc_fixable, img_woc_job);
@@ -52,7 +53,7 @@ function render_option(fixable, img_job){
             if(jobs[index].checked){
                 img_job.innerHTML = '';
                 var option = document.createElement('option');
-                option.text = jobs[index].getAttribute('text');
+                option.text = jobs[index].dataset.text;
                 option.value = jobs[index].getAttribute('value');
                 elem.add(option);
             }
--- jgm/static/js/party.js
+++ jgm/static/js/party.js
@@ -1,17 +1,20 @@
-var img_zone_job = document.getElementById('party_card_body');
+var party_card_body = document.getElementById('party_card_body');
+var party_btn_edit = document.getElementById('party_btn_edit');
 
 function party_edit(e){
-console.log('edit')
+console.log('edit');
 };
 
 function party_save(e){
-console.log('save')
+console.log('save');
 };
 
 function party_back(e){
-console.log('back')
+console.log('back');
 };
 
 function party_remove(e){
-console.log('remove')
+console.log('remove');
+console.log(e.dataset.party_name);
+confirm("Delete party " + e.dataset.party_name + "?");
 };
